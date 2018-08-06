84b74046107ea0f5f9d0ead9b5bd9cde754179c9
naj147 <abdelhadi.najih@um5s.net.ma>
2018-08-06 21:33:25 +0300

    Testing Added to some modules

---
+++ datalayer/src/test/java/com/example/datalayer/repository/datasource/PhotosCloudDataStoreTest.java
@@ -0,0 +1,15 @@
+package com.example.datalayer.repository.datasource;
+
+import com.example.datalayer.cache.PhotosCache;
+import com.example.datalayer.net.API;
+
+import org.mockito.Mock;
+
+public class PhotosCloudDataStoreTest {
+    @Mock
+    API apiService;
+    @Mock
+    PhotosCache photosCache;
+
+
+}
